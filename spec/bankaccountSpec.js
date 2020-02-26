9074101802dd9545f15363af29c52404e5e7ac97
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-26 22:08:36 +0000

    Refactors tests to use beforeEach to DRY up code

--- spec/bankaccountSpec.js
+++ spec/bankaccountSpec.js
@@ -1,13 +1,15 @@
 describe('BankAccount', function() {
   var bankaccount;
 
+  beforeEach(function() {
+    bankaccount = new BankAccount();
+  });
+  
   it ('is initialized with a balance of 2000', function(){
-    bankaccount = new BankAccount
     expect(bankaccount.balance).toEqual(2000)
   });
 
   it('the account holder can withdraw money', function() {
-    bankaccount = new BankAccount
     bankaccount.withdraw(20)
     expect(bankaccount.balance).toEqual(1980)
   });
