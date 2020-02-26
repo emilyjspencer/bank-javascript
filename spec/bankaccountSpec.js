afd3a8205b99ab325c80c1510ae098648e417446
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-26 22:05:57 +0000

    Account holder can withdraw money

--- spec/bankaccountSpec.js
+++ spec/bankaccountSpec.js
@@ -4,6 +4,11 @@ describe('BankAccount', function() {
   it ('is initialized with a balance of 2000', function(){
     bankaccount = new BankAccount
     expect(bankaccount.balance).toEqual(2000)
+  });
 
+  it('the account holder can withdraw money', function() {
+    bankaccount = new BankAccount
+    bankaccount.withdraw(20)
+    expect(bankaccount.balance).toEqual(1980)
   });
 });
--- src/bankaccount.js
+++ src/bankaccount.js
@@ -1,3 +1,7 @@
 function BankAccount(){
   this.balance = 2000;
 }
+
+BankAccount.prototype.withdraw = function(amount) {
+  this.balance -= amount
+};
